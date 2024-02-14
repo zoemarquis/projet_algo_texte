fc145a3b49cd6f00564a9c660fe3a96365fd555f
MASSEBEUF Valentin <74383437+gargaranza@users.noreply.github.com>
2024-02-14 22:55:58 +0100

    Update analyse.py

--- scripts/analyse.py
+++ scripts/analyse.py
@@ -1,18 +1,11 @@
 
-def analyse_bornes(txt, borne_min, borne_max):
+def transforme_bornes(txt, borne_min, borne_max):
     tmp = txt.split('..')
     if len(tmp) != 2:
         return 0
-
-    try:
-        borne_inf = int(tmp[0])
-    except ValueError:
-        return 0
-            
-    try:
-        borne_sup = int(tmp[1])
-    except ValueError:
-        return 0
+        
+    borne_inf = int(tmp[0])
+    borne_sup = int(tmp[1])
         
     if borne_inf < borne_sup and borne_inf > borne_min and borne_sup < borne_max:
         return [(borne_inf, borne_sup)]
@@ -32,7 +25,7 @@ def analyse_join(txt, borne_max):
     borne_courante = 0
     
     for borne in txt.split(','):
-        res = analyse_bornes(borne, borne_courante, borne_max)
+        res = transforme_bornes(borne, borne_courante, borne_max)
         if not res: return 0
         
         bornes += res
@@ -49,7 +42,7 @@ def analyse_complement(txt, borne_max):
         return 0
     txt = tmp[0]
 
-    res = analyse_bornes(txt, 0, borne_max)
+    res = transforme_bornes(txt, 0, borne_max)
     if not res: return 0
 
     return res
@@ -68,7 +61,7 @@ def analyse_complement_join(txt, borne_max):
     borne_courante = 0
 
     for borne in txt.split(','):
-        res = analyse_bornes(borne, borne_courante, borne_max)
+        res = transforme_bornes(borne, borne_courante, borne_max)
         if not res: return 0
         
         bornes += res
@@ -95,7 +88,7 @@ def analyne_bornes(txt, borne_max):
             #get_complement(bornes)
             return bornes
     else:
-        bornes = analyse_bornes(txt, 0, borne_max)
+        bornes = transforme_bornes(txt, 0, borne_max)
         if bornes:
             #get_bornes(bornes)
             return bornes

4035fbae864273f3906bfc986737ebe6182c8519
MartinDeniau <martin.deniau@etu.unistra.fr>
2024-02-19 08:21:29 +0100

    Factorisation sequence.py et transition vers analyse.py

--- scripts/analyse.py
+++ scripts/analyse.py
@@ -1,18 +1,23 @@
-
 def transforme_bornes(txt, borne_min, borne_max):
-    tmp = txt.split('..')
+    tmp = txt.split(':')
     if len(tmp) != 2:
         return 0
-        
-    borne_inf = int(tmp[0])
-    borne_sup = int(tmp[1])
-        
+    try:
+        borne_inf = int(tmp[0])
+        borne_sup = int(tmp[1])
+    except:
+        print("Parsing Error")
+        return 0
+
     if borne_inf < borne_sup and borne_inf > borne_min and borne_sup < borne_max:
+        print("borne_inf", borne_inf, " borne_sup", borne_sup)
         return [(borne_inf, borne_sup)]
     else:
         return 0
 
+
 def analyse_join(txt, borne_max):
+    print("Parsing Join")
     tmp = txt.split('join(')
     if len(tmp) != 2:
         return 0
@@ -20,20 +25,22 @@ def analyse_join(txt, borne_max):
     if len(tmp) != 2:
         return 0
     txt = tmp[0]
-    
+
     bornes = []
     borne_courante = 0
-    
+
     for borne in txt.split(','):
         res = transforme_bornes(borne, borne_courante, borne_max)
         if not res: return 0
-        
+
         bornes += res
         borne_courante = res[0][1]
 
     return bornes
 
+
 def analyse_complement(txt, borne_max):
+    print("Parsing Complement")
     tmp = txt.split('complement(')
     if len(tmp) != 2:
         return 0
@@ -49,6 +56,7 @@ def analyse_complement(txt, borne_max):
 
 
 def analyse_complement_join(txt, borne_max):
+    print("Parsing Complement Join")
     tmp = txt.split('complement(join(')
     if len(tmp) != 2:
         return 0
@@ -63,33 +71,32 @@ def analyse_complement_join(txt, borne_max):
     for borne in txt.split(','):
         res = transforme_bornes(borne, borne_courante, borne_max)
         if not res: return 0
-        
+
         bornes += res
         borne_courante = res[0][1]
 
     return bornes
 
 
-def analyne_bornes(txt, borne_max):
+def analyse_bornes(txt, borne_max):
     bornes = []
     if txt[:4] == 'join':
         bornes = analyse_join(txt, borne_max)
         if bornes:
-            #get_join(bornes)
+            # get_join(bornes)
             return bornes
     elif txt[:15] == 'complement(join':
         bornes = analyse_complement_join(txt, borne_max)
         if bornes:
-            #get_complement_join(bornes)
+            # get_complement_join(bornes)
             return bornes
     elif txt[:10] == 'complement':
         bornes = analyse_complement(txt, borne_max)
         if bornes:
-            #get_complement(bornes)
+            # get_complement(bornes)
             return bornes
     else:
         bornes = transforme_bornes(txt, 0, borne_max)
         if bornes:
-            #get_bornes(bornes)
+            # get_bornes(bornes)
             return bornes
-
--- scripts/sequence.py
+++ scripts/sequence.py
@@ -1,15 +1,27 @@
 from Bio import Entrez, SeqIO
+import analyse
 
-Entrez.email = "martin.deniau@etu.unistra.fr"
 
-handle = Entrez.esearch(db="nucleotide", term="NC", retmax ="1", usehistory='y', idtype="acc")
-record = Entrez.read(handle)
+def search(domain, name):
+    Entrez.email = "martin.deniau@etu.unistra.fr"
 
-print("Records found:", record["IdList"])
+    handle = Entrez.esearch(db="nucleotide", term="("+name+"["+domain+"]"+") AND (NC_*[Accession])", retmax ="1")
 
-handle_fetch = Entrez.efetch(db="nucleotide", id=record["IdList"], rettype="gbwithparts", retmode="text")
-record_fetch = SeqIO.read(handle_fetch, "genbank")
-#handle_fetch = Entrez.efetch(db="nuccore", id=record["IdList"], rettype="fasta")
-#record_fetch = SeqIO.read(handle_fetch, "fasta")
-#print(record_fetch)
-print(record_fetch.seq)
+    record = Entrez.read(handle)
+
+    print("Records found:", record["IdList"])
+
+    return record["IdList"]
+
+
+def fetch(ids, regions):
+    for id in ids:
+        print("Fetching sequence", id)
+        handle = Entrez.efetch(db="nucleotide", id=id, rettype="gbwithparts", retmode="text")
+        print("Fetched")
+        for record in SeqIO.parse(handle, "gb"):
+            for feature in record.features:
+                for region in regions:
+                    if feature.type == region:
+                        analyse.analyse_bornes(str(feature.location)[1:-4], len(record.seq))
+    return 0

03c0701cb67d5fa118834ffa5786f2ba6df5930f
MartinDeniau <martin.deniau@etu.unistra.fr>
2024-02-21 13:37:17 +0100

    Correction des paramètres de recherche

--- scripts/sequence.py
+++ scripts/sequence.py
@@ -5,7 +5,7 @@ import analyse
 def search(domain, name):
     Entrez.email = "martin.deniau@etu.unistra.fr"
 
-    handle = Entrez.esearch(db="nucleotide", term="("+name+"["+domain+"]"+") AND (NC_*[Accession])", retmax ="1")
+    handle = Entrez.esearch(db="nucleotide", term="("+name+"["+domain+"]"+") AND (NC_*[Accession])", retmax ="9999")
 
     record = Entrez.read(handle)
 
@@ -24,4 +24,3 @@ def fetch(ids, regions):
                 for region in regions:
                     if feature.type == region:
                         analyse.analyse_bornes(str(feature.location), len(record.seq))
-    return 0

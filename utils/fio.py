b6799f07393f48856691d83199cc943556623fc7
MartinDeniau <martin.deniau@etu.unistra.fr>
2024-02-28 16:29:28 +0100

    Correction adresse fichier

--- utils/fio.py
+++ utils/fio.py
@@ -2,6 +2,7 @@ import requests
 
 
 def request_kingdom(kingdom):
-    request = requests.get("https://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/IDS/" + kingdom + ".ids")
+    request = requests.get("https://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/IDS/" + kingdom[0].upper() + kingdom[1:] + ".ids")
     with open(kingdom + ".txt", "wb") as f:
         f.write(request.content)
+

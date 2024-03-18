31ca9b23be22d5bca8609c2aeab6673b903115ff
Chaïma Jaïdane <cjaidane@etu.unistra.fr>
2024-03-18 10:58:27 +0100

    theme ajout de couleur

--- graphique/theme.py
+++ graphique/theme.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 from tkinter import ttk 
 
-couleur_frame = "#535878"  
-#556C96"#"#516079"#BE9CC7"#535878"  
-couleur_fond = "#1B3358"
-#516079#"#556C96"#1C2942"#1B3358"  
+couleur_frame = "#EDC18D"
+#535878" #556C96"#"#516079"#BE9CC7"#535878"  
+couleur_fond = "#313950"
+#1B3358"#516079#"#556C96"#1C2942"#1B3358"  
 couleur_texte = "white"  
 couleur_selection="red"
 
